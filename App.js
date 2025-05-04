829691cf40b82b2fbef251a1c2c4edbded765b7d
kirthykaa <114657054+Kirthykaa@users.noreply.github.com>
2025-05-04 06:45:57 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import Home from './home';
+import About from './about';
+import Contact from './contact';
+import Main from './main';
+
+function App() {
+  return (
+    <Router history={createBrowserHistory()}>
+      <Routes>
+        <Route path='' element={<Main />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
